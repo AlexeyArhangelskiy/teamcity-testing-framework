70764f270328c7a0647f7a9d887dbdbf34eb2eea
Aleksei Arkhangelskii <arhmost@gmail.com>
2025-03-02 21:51:28 +0100

    add checked and unchecked

---
+++ src/main/java/com/example/teamcitydemo/api/requests/unchecked/UncheckedBase.java
@@ -0,0 +1,50 @@
+package com.example.teamcitydemo.api.requests.unchecked;
+
+import com.example.teamcitydemo.api.enums.Endpoint;
+import com.example.teamcitydemo.api.models.BaseModel;
+import com.example.teamcitydemo.api.requests.CrudInterface;
+import com.example.teamcitydemo.api.requests.Request;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class UncheckedBase extends Request implements CrudInterface {
+
+    public UncheckedBase(RequestSpecification spec, Endpoint endpoint) {
+        super(spec, endpoint);
+    }
+
+    @Override
+    public Response create(BaseModel model) {
+        return RestAssured
+                .given()
+                .spec(spec)
+                .body(model)
+                .post(endpoint.getUrl());
+    }
+
+    @Override
+    public Response read(String id) {
+        return RestAssured
+                .given()
+                .spec(spec)
+                .get(endpoint.getUrl() + "/id:" + id);
+    }
+
+    @Override
+    public Response update(String id, BaseModel model) {
+        return RestAssured
+                .given()
+                .spec(spec)
+                .body(model)
+                .put(endpoint.getUrl() + "/id:" + id);
+    }
+
+    @Override
+    public Response delete(String id) {
+        return RestAssured
+                .given()
+                .spec(spec)
+                .delete(endpoint.getUrl() + "/id:" + id);
+    }
+}
