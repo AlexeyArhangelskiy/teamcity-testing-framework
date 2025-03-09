7de1ec134b51370ca59c7bfb6ded011b4fee653c
Aleksei <arhmost@gmail.com>
2025-03-09 19:48:48 +0300

    add test data

--- src/test/java/com/example/teamcitydemo/api/BaseTest.java
+++ src/test/java/com/example/teamcitydemo/api/BaseTest.java
@@ -1,18 +1,23 @@
 package com.example.teamcitydemo.api;
 
+import com.example.teamcitydemo.api.models.TestData;
 import com.example.teamcitydemo.api.requests.CheckedRequests;
 import com.example.teamcitydemo.api.spec.Specifications;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.asserts.SoftAssert;
 
+import static com.example.teamcitydemo.api.generators.TestDataGenerator.generate;
+
 public class BaseTest {
     protected SoftAssert softy;
     protected CheckedRequests superUserCheckRequests = new CheckedRequests(Specifications.superUserAuth());
+    protected TestData testData;
 
     @BeforeMethod(alwaysRun = true)
     public void beforeTest() {
         softy = new SoftAssert();
+        testData = generate();
     }
 
     @AfterMethod(alwaysRun = true)
